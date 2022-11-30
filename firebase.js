f58e727dd9be0c6093f8e5ccb150471e66a83ae3
Abrajam7611 <luis.vel4x@gmail.com>
2022-11-30 11:58:10 -0600

    La mayoria

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, getDoc, updateDoc,} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-import {getStorage, uploadBytes, ref,} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+import { getFirestore, collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, getDoc, updateDoc, } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { getStorage, uploadBytes, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,20 +14,19 @@ const firebaseConfig = {
   appId: "1:1019437127615:web:5bf9dd5dcccd6c19d7730a"
 };
 
- // Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore();
 const storage = getStorage();
 
 
-export const saveTask = (title, description) => 
-  addDoc(collection(db, "tasks"), {title, description});
+export const saveTask = (title, description, imageUrl) => addDoc(collection(db, 'tasks'), { title, description, imageUrl });
 
 export const getTasks = () => getDocs(collection(db, 'tasks'));
 
-export const onGetTasks = (callback) => 
-  onSnapshot(collection(db, 'tasks'),callback);
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(db, 'tasks'), callback);
 
 export const deleteTasks = id => deleteDoc(doc(db, 'tasks', id));
 
@@ -37,12 +35,34 @@ export const getTask = id => getDoc(doc(db, 'tasks', id));
 export const updateTasks = (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);
 
 export const saveImage = file => {
-console.log(file);
-const storageRef = ref(storage, `images/${file.name}`);
+  console.log(file);
+  const storageRef = ref(storage, `images/${file.name}`);
 
-uploadBytes(storageRef, file).then((snapshot) => {
-  console.log('Uploaded a blob or file');
-});
+  const uploadTask = uploadBytesResumable(storageRef, file);
 
-  
+  // Register three observers:
+  // 1. 'state_changed' observer, called any time the state changes
+  // 2. Error observer, called on failure
+  // 3. Completion observer, called on successful completion
+  uploadTask.on('state_changed',
+    (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      document.querySelector('#progress').value = progress;
+    },
+    (error) => {
+      // Handle unsuccessful uploads
+    },
+    () => {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        //document.querySelector('#progress').value = 'Fin!';
+        document.querySelector('#image').src = downloadURL;
+        console.log('File available at', downloadURL);
+      });
+    }
+  );
+
 }
